02111ff554656a352d588a86b41b0369661bbd5a
Evangelos Karachristos <vagelis95@hotmail.com>
2023-10-23 16:42:05 +0300

    Add annotations on login request

--- Athletes.Info/Request/AthleteLoginRequest.cs
+++ Athletes.Info/Request/AthleteLoginRequest.cs
@@ -1,9 +1,14 @@
 ﻿using Athletes.Info.Model;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Athletes.Info.Request
 {
     public record AthleteLoginRequest : AthleteInfoBase
     {
+        [Required]
+        [MaxLength(16)]
+        [JsonPropertyName("password")]
         public string Password { get; set; }
     }
 }
